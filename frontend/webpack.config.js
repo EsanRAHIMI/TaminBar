dc75cf5e8680250900c8d665ff99571657b68961
Esan <ehsanrahimi@live.com>
2024-12-26 19:02:38 +0400

    Fix NODE_ENV conflicts

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       systemvars: true,
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
   ],
   devServer: {
